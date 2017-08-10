21966e172f8e5934209df3dc200ec347c1b8e5be
allefpablo <allefpablo@gmail.com>
2017-08-10 10:43:14 -0300

    Update in main class

--- flash_firmware.py
+++ flash_firmware.py
@@ -1,11 +1,33 @@
 import os
 
+class LoadFirmware:
 
-def flash_udc_firmware(ARM_filename,  C28_filename):
-	print("flashing firmware...")
-	try:
-		os.system(" CMD /C \"ccs_base\\DebugServer\\bin\\DSLite\" flash -c user_files/configs/f28m36p63c2.ccxml -l user_files/settings/generated.ufsettings -e -f -v user_files/images/" + ARM_filename)
-		os.system(" CMD /C \"ccs_base\\DebugServer\\bin\\DSLite\" flash -c user_files/configs/f28m36p63c2.ccxml -l user_files/settings/generated.ufsettings --core=1 -e -f -v user_files/images/" + C28_filename)
-		print("FIRWARE GRAVADO.")
-	except:
-		print("ERRO AO GRAVAR FIRMWARE!")
+    def __init__(self, arm_filename=None, c28_filname=None):
+
+        self._arm = arm_filename
+        self._c28 = c28_filname
+
+    @property
+    def arm_filename(self):
+        return self._arm
+
+    @arm_filename.setter
+    def arm_filename(self, path):
+        self._arm = path
+
+    @property
+    def c28_filename(self):
+        return self._c28
+
+    @c28_filename.setter
+    def c28_filename(self, path):
+        self._c28 = path
+
+    def flash_firmware(self):
+        print("flashing firmware...")
+        try:
+        	os.system(" CMD /C \"ccs_base\\DebugServer\\bin\\DSLite\" flash -c user_files/configs/f28m36p63c2.ccxml -l user_files/settings/generated.ufsettings -e -f -v user_files/images/" + self._arm)
+        	os.system(" CMD /C \"ccs_base\\DebugServer\\bin\\DSLite\" flash -c user_files/configs/f28m36p63c2.ccxml -l user_files/settings/generated.ufsettings --core=1 -e -f -v user_files/images/" + self._c28)
+        	print("FIRWARE GRAVADO.")
+        except:
+        	print("ERRO AO GRAVAR FIRMWARE!")
--- test_flash.py
+++ test_flash.py
@@ -1,7 +1,14 @@
 from flash_firmware import *
 
-flash_udc_firmware("M3_blink_slow.out", "C28_blink_slow.out")
-wait = input("DESLIGUE E LIGUE O UDC.")
+flash = LoadFirmware()
 
-flash_udc_firmware("M3_blink_fast.out", "C28_blink_fast.out")
-wait = input("DESLIGUE E LIGUE O UDC.")
+flash.arm_filename = "M3_blink_slow.out"
+flash.c28_filname = "C28_blink_slow.out"
+
+flash.flash_firmware()
+
+#flash_udc_firmware("M3_blink_slow.out", "C28_blink_slow.out")
+#wait = input("DESLIGUE E LIGUE O UDC.")
+#
+#flash_udc_firmware("M3_blink_fast.out", "C28_blink_fast.out")
+#wait = input("DESLIGUE E LIGUE O UDC.")
